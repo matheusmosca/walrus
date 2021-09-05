6a5bb42a357a5a685d5222f19cb92e53b13e3aad
Matheus Mosca <matheusmoscag@gmail.com>
2021-09-05 14:54:24 -0300

    feat: add v1 subscribe rpc method

---
+++ rpc/v1/subscribe.go
@@ -0,0 +1,30 @@
+package rpc
+
+import (
+	"context"
+
+	"github.com/matheusmosca/walrus/domain/vos"
+	pb "github.com/matheusmosca/walrus/proto"
+)
+
+func (r RPC) Subscribe(req *pb.SubscribeRequest, stream pb.Walrus_SubscribeServer) error {
+	subscriptionCh, err := r.useCase.Subscribe(context.TODO(), req.ConsumerName, vos.TopicName(req.Topic))
+	if err != nil {
+		return err
+	}
+
+	for msg := range subscriptionCh {
+		res := &pb.SubscribeResponse{
+			Message: &pb.Message{
+				Topic:       msg.TopicName.String(),
+				Body:        msg.Body,
+				PublishedBy: msg.PublishedBy,
+			},
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
