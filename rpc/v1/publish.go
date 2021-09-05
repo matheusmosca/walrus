ef5699cd235d897c47d7dce08bb7e78646bf5cf4
Matheus Mosca <matheusmoscag@gmail.com>
2021-09-05 17:07:12 -0300

    chore: add logs on publish method

--- rpc/v1/publish.go
+++ rpc/v1/publish.go
@@ -7,9 +7,19 @@ import (
 
 	"github.com/matheusmosca/walrus/domain/vos"
 	pb "github.com/matheusmosca/walrus/proto"
+	"github.com/sirupsen/logrus"
 )
 
 func (r RPC) Publish(ctx context.Context, req *pb.PublishRequest) (*emptypb.Empty, error) {
+	const operation = "RPC.Publish"
+
+	log := r.log.WithContext(ctx).WithFields(logrus.Fields{
+		"publishes_by": req.Message.PublishedBy,
+		"topic_name":   req.Message.Topic,
+		"operation":    operation,
+	})
+	log.Info("starting to handle new publish request")
+
 	msg := vos.Message{
 		PublishedBy: req.Message.PublishedBy,
 		TopicName:   vos.TopicName(req.Message.Topic),
@@ -18,8 +28,10 @@ func (r RPC) Publish(ctx context.Context, req *pb.PublishRequest) (*emptypb.Empt
 
 	err := r.useCase.Publish(ctx, msg)
 	if err != nil {
+		log.WithError(err).Error("could not handle publish request")
 		return nil, err
 	}
+	log.Info("message has been published successfully")
 
 	return &emptypb.Empty{}, nil
 }
