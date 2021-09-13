0f99d5f7cd06beb3cf826b8ecb4ad3d6a790689b
Olsi Bakullari <olsi.bakullari@lyke.de>
2021-09-13 22:46:36 +0200

    fix: Use a sync.Mutex to avod concurrency issues when creating a new topic

--- repositories/memory/topics/create_topic.go
+++ repositories/memory/topics/create_topic.go
@@ -7,12 +7,14 @@ import (
 	"github.com/matheusmosca/walrus/domain/vos"
 )
 
-func (r repository) CreateTopic(ctx context.Context, name vos.TopicName, topic entities.Topic) error {
+func (r *repository) CreateTopic(ctx context.Context, name vos.TopicName, topic entities.Topic) error {
 	if _, ok := r.storage[name]; ok {
 		return entities.ErrTopicAlreadyExists
 	}
 
+	r.mu.Lock()
 	r.storage[name] = topic
+	r.mu.Unlock()
 
 	return nil
 }
